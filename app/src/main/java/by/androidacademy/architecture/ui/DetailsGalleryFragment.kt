99a39ed468433f091bee8c803cc3f0fd95c476ea
Aliaksei Trafimchyk <aliaksei.trafimchyk@gmail.com>
2020-02-15 22:55:42 +0300

    ViewModel: integrated MoviesViewModel into DetailsGalleryFragment.

--- app/src/main/java/by/androidacademy/architecture/ui/DetailsGalleryFragment.kt
+++ app/src/main/java/by/androidacademy/architecture/ui/DetailsGalleryFragment.kt
@@ -1,20 +1,20 @@
 package by.androidacademy.architecture.ui
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import by.androidacademy.architecture.Dependencies
 import by.androidacademy.architecture.R
+import by.androidacademy.architecture.presentation.MoviesViewModel
+import by.androidacademy.architecture.presentation.MoviesViewModelFactory
 import by.androidacademy.architecture.ui.adapters.DetailsFragmentAdapter
-import by.androidacademy.architecture.domain.usecase.GetMoviesResult
-import by.androidacademy.architecture.domain.usecase.GetPopularMoviesUseCase
 import kotlinx.android.synthetic.main.fragment_movies_gallery.*
 
-class DetailsGalleryFragment : Fragment() {
+class DetailsGalleryFragment : Fragment(R.layout.fragment_movies_gallery) {
 
     companion object {
 
@@ -27,15 +27,15 @@ class DetailsGalleryFragment : Fragment() {
         }
     }
 
-    private val getPopularMoviesUseCase: GetPopularMoviesUseCase =
-        Dependencies.getPopularMoviesUseCase
+    private val viewModelFactory: MoviesViewModelFactory = Dependencies.moviesViewModelFactory
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_movies_gallery, container, false)
+    private lateinit var viewModel: MoviesViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        viewModel = ViewModelProvider(this, viewModelFactory)
+            .get(MoviesViewModel::class.java)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -43,22 +43,14 @@ class DetailsGalleryFragment : Fragment() {
 
         val position = arguments?.getInt(ARGS_MOVIE_POSITION) ?: 0
 
-        getPopularMoviesUseCase.getMovies { result ->
-            when (result) {
-                is GetMoviesResult.Success -> {
-                    vp_pager.run {
-                        adapter = DetailsFragmentAdapter(childFragmentManager, result.movies)
-                        currentItem = position
-                    }
-                }
-                is GetMoviesResult.Error -> {
-                    Toast.makeText(
-                        requireContext(),
-                        result.message,
-                        Toast.LENGTH_LONG
-                    ).show()
-                }
+        viewModel.moviesLiveData.observe(viewLifecycleOwner, Observer { movies ->
+            vp_pager.run {
+                adapter = DetailsFragmentAdapter(childFragmentManager, movies)
+                currentItem = position
             }
-        }
+        })
+        viewModel.errorLiveData.observe(viewLifecycleOwner, Observer { message ->
+            Toast.makeText(requireContext(), message, Toast.LENGTH_LONG).show()
+        })
     }
 }
